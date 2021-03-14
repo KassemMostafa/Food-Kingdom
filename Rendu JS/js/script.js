577a34fba0959995e0051d2aca8dab6f0eadbfd8
KassemMostafa <mostafa.akassem03@gmail.com>
2021-03-14 17:49:39 +0100

    Affichage stock ajouté

---
+++ Rendu JS/js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+function affichageStock() 
+{
+    var stock, i;
+
+    stock = document.getElementsByClassName("stock");
+    if (document.getElementById("stock-button").value == 'stock-cache') {                 
+        for (i = 0; i < stock.length; i++) {
+            stock[i].style.display = "inline";
+            stock[i].style.textAlign = "right";
+        }
+        document.getElementById("stock-button").innerHTML="Cacher Stock";
+        document.getElementById("stock-button").value="stock-affiche";
+    } else
+    {
+        for (i = 0; i < stock.length; i++) {
+            stock[i].style.display = "none";
+        }
+        document.getElementById("stock-button").innerHTML="Afficher Stock";
+        document.getElementById("stock-button").value="stock-cache";
+        
+    }
+}
