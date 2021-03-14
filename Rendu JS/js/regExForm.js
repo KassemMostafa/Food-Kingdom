12da6eab9595779ba6777aa59fc8fcfc94c8738d
KassemMostafa <mostafa.akassem03@gmail.com>
2021-03-14 23:25:49 +0100

    rendu

--- Rendu JS/js/regExForm.js
+++ Rendu JS/js/regExForm.js
@@ -12,30 +12,6 @@ form.pnom.addEventListener('change', function () {
     validpnom(this);
 }
 );
-form.dateNaissance.addEventListener('change', function () {
-    let auj = new Date();
-    let jour = today.getDate();
-    let mois = today.getMonth() + 1;
-    let annee = today.getyear();
-
-    if (jour < 10) {
-        jour = '0' + jour;
-    }
-    if (mois < 10) {
-        mois = '0' + mois;
-    }
-    auj = annee + '-' + mois + '-' + jour;
-    form.date.setAttribute("max", auj);
-}
-);
-/*
-form.email.addEventListener('change', function () {
-    validemail(this);
-}
-);
-*/
-//form.nom.
-
 const validnom = function (input) {
     let nomRegExp = new RegExp("^[A-Z][A-Za-z\�\�\�\-]+$", "g");
     let test = nomRegExp.test(input.value);
@@ -45,8 +21,6 @@ const validnom = function (input) {
         small.innerHTML = "";
     }
     else {
-        //innerHTML = "<br/>";
-       // form.nom.innerHTML = "<br>";
         form.nom.style.borderColor = "red";
         small.innerHTML = "<br>Votre prenom doit commencer par une majuscule et comporter ensuite une minuscule et ne peut contenir de caractere spetiaux";
     }
@@ -68,42 +42,19 @@ const validpnom = function (input) {
     console.log(test);
 };
 
-/*
-const validemail = function (input) {
-    let nomRegExp = new RegExp("^[A-Z][A-Za-z\�\�\�\-]+$", "g");
-    let test = nomRegExp.test(input.value);
-    let small = input.nextElementSibling;
-    if (test) {
-        form.email.style.borderColor = "green";
-        small.innerHTML = "";
+const verifDate = function (){
+    let ajd = new Date();
+    let jour = ajd.getDate();
+    let mois = ajd.getMonth() + 1; //January is 0!
+    let annee = ajd.getFullYear();
+    if (jour < 10) {
+        jour = '0' + jour;
     }
-    else {
-        //innerHTML = "<br/>";
-        // form.nom.innerHTML = "<br>";
-        form.email.style.borderColor = "red";
-        small.innerHTML = "Votre prenom doit commencer par une majuscule et comporter ensuite une minuscule";
+    if (mois < 10) {
+        mois = '0' + mois;
     }
-    console.log(test);
-};*/
-
-/*
-const verifDate = function (var){
-    let today = new Date();
-    let dd = today.getDate();
-    let mm = today.getMonth() + 1; //January is 0!
-    let yyyy = today.getFullYear();
-    if (dd < 10) {
-        dd = '0' + dd;
-    }
-    if (mm < 10) {
-        mm = '0' + mm;
-    }
-    today = yyyy + '-' + mm + '-' + dd;
-document.getElementById("dateNaissance").setAttribute("max", today);
+    ajd = annee + '-' + mois + '-' + jour;
+    form.dateNaissance.setAttribute("max", ajd);
 };
-*/
 
-/*let nom = document.getElementById('nom').value;
-console.log(nom);
-//document.getElementById('nom').value;
-*/
+verifDate();
