cae4a42bfdb604fbaf902a631cf2ad18b0318660
KassemMostafa <mostafa.akassem03@gmail.com>
2021-04-02 12:48:12 +0200

    include navbar

--- Rendu PHP/contact.php
+++ Rendu PHP/contact.php
@@ -12,37 +12,7 @@
 </head>
 <!-- Entete Navbar non affiché sur les petites écrans due au manque du javascript, erreur à réparer lors du deuxième rendu-->
 <header>
-    
-    <nav class="nav navbar-expand-lg navbar-dark bg-dark ">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="Index.html">
-                <img id="logo" class=" d.inline-block align-middle" alt="logo" name="logo" src="img/cuteBurger.png" height="40" />
-                Food Kingdom
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-                <ul class="navbar-nav justify-content-center">
-                    <li class="nav-item">
-                        <a class="nav-link" href="Index.html">Accueil</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="burger.html">Burger</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="poulet.html">Poulet</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="pizza.html">Pizza</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="contact.html">Contact</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php include("php\header.php"); ?>
 </header>
 
 <body>
---
+++ Rendu PHP/php/header.php
@@ -0,0 +1,28 @@
+<nav class="nav navbar-expand-lg navbar-dark bg-dark ">
+		<div class="container-fluid">
+		  <a class="navbar-brand" href="Index.html"><img id="logo" class=" d.inline-block align-middle" alt="logo" name="logo" src="img/cuteBurger.png" height="40"/>
+			Food Kingdom</a>
+		  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+			<ul class="navbar-nav justify-content-center">
+			  <li class="nav-item">
+				<a class="nav-link active" aria-current="page" href="Index.html">Accueil</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="burger.php">Burger</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="poulet.php">Poulet</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="pizza.php">Pizza</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="contact.php">Contact</a>
+			  </li>
+			</ul>
+		  </div>
+		</div> 
+	  </nav>
