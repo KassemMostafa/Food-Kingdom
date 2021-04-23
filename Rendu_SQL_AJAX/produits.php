4b4b724862d5f9e43a85aa75e89b95e1a1e2ee8d
KassemMostafa <mostafa.akassem03@gmail.com>
2021-04-23 14:21:49 +0200

    fixed js

--- Rendu_SQL_AJAX/produits.php
+++ Rendu_SQL_AJAX/produits.php
@@ -51,8 +51,8 @@
 					
 					<div class="col col-md-auto">
 						<form method="POST" action="php/incrementation.php" enctype="multipart/form-data">
-							<div class="card bg-dark text-light" id="<?php echo $produit["alt"]?>" style="width: 18rem";>
-								<img src = "<?php echo $produit['image']?>" class = "card-img-top zoom" alt = "<?php echo $produit["alt"] ?>">
+							<div class="card bg-dark text-light" id="<?php echo $value["alt"]?>" style="width: 18rem";>
+								<img src = "<?php echo $value['image']?>" class = "card-img-top zoom" alt = "<?php echo $value["alt"] ?>">
 								<input name="alt" type="hidden" value="<?php echo $value["alt"]?>">
 								<input name="produit" type="hidden" value="<?php echo $value["nom"]?>">
 								<div class="card-body">
